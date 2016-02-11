ab2fc38d78ed1fd1790a1d98124920bcc21798e8
IvanMladenov <ismladenov@gmail.com>
2016-02-11 15:45:36 +0200

    Problem 02 bug fix level 2

--- SimulatingOOPinJS/P02_BugFixLevel2/levelTwo.js
+++ SimulatingOOPinJS/P02_BugFixLevel2/levelTwo.js
@@ -1,17 +1,44 @@
 var Person = (function(){
     function Person(firstName, lastName){
-        this.name = firstName + ' ' + lastName;
         this.firstName = firstName;
         this. lastName = lastName;
+
+        Object.defineProperty(this, 'name', {
+            get:function(){
+                return this.firstName + ' ' + this.lastName;
+            },
+            set: function(name) {
+                var names = name.split(' ');
+                this.firstName = names[0];
+                this.lastName = names[1];
+            }
+        })
+
     }
 
     return Person;
 })();
 
-var peter = new Person("Peter", "Jackson");
+var person = new Person("Peter", "Jackson");
+console.log(person.name);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log();
 
-peter.firstName = 'Gosho';
-console.log(peter.name);
-console.log(peter.firstName);
-console.log(peter.lastName);
+person.firstName = "Ivan";
+console.log(person.name);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log();
+
+person.name = 'Donald Duck';
+console.log(person.name);
+console.log(person.firstName);
+console.log(person.lastName);
+console.log();
+
+person.lastName = 'Shmruk';
+console.log(person.name);
+console.log(person.firstName);
+console.log(person.lastName);
 
